874c89fbe291099a836cabc24d0b02f3ff64baa0
Murat Altun <mraltun@gmail.com>
2023-01-31 13:46:00 +0300

    Better name convention

--- 1) Layout Components/a) Split Screen/SplitScreen.jsx
+++ 1) Layout Components/a) Split Screen/SplitScreen.jsx
@@ -8,9 +8,8 @@ const Pane = styled.div`
   flex: ${(props) => props.weight};
 `;
 
-export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 2 }) => {
+export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
   const [left, right] = children;
-
   return (
     <Container>
       <Pane weight={leftWeight}>{left}</Pane>
