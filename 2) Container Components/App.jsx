4a0c44d2fcb0e6a548f2dd6d95829271f9932c19
Murat Altun <mraltun@gmail.com>
2023-02-01 13:01:02 +0300

    Added DataSource component

--- 2) Container Components/App.jsx
+++ 2) Container Components/App.jsx
@@ -1,8 +1,15 @@
+import axios from "axios";
 import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
 import { UserLoader } from "./UserLoader";
 import { ResourceLoader } from "./ResourceLoader";
+import { DataSource } from "./DataSource";
 import ProductInfo from "./ProductInfo";
+
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 const App = () => {
   return (
@@ -23,6 +30,9 @@ const App = () => {
       <ResourceLoader resourceUrl='/products/1234' resourceName='product'>
         <ProductInfo />
       </ResourceLoader>
+      <DataSource getDataFunc={getServerData("/user/123")} resourceName='user'>
+        <UserInfo />
+      </DataSource>
     </>
   );
 };
