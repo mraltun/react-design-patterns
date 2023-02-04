bbc729e02460429e475abe0d98bc5994a31401fa
Murat Altun <mraltun@gmail.com>
2023-02-04 12:28:23 +0300

    Added withUser and loading data

--- 4) Higher-Order Components/App.jsx
+++ 4) Higher-Order Components/App.jsx
@@ -1,10 +1,17 @@
 import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
+import { withUser } from "./withUser";
 
 const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader = withUser(UserInfo, "234");
 
 const App = () => {
-  return <UserInfoWrapped a={1} b='Hello' c={{ name: "Murat" }} />;
+  return (
+    <>
+      <UserInfoWrapped a={1} b='Hello' c={{ name: "Murat" }} />
+      <UserInfoWithLoader />
+    </>
+  );
 };
 
 export default App;
