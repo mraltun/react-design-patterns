123bd6d5e36df44f5ca7439aa119fe2292b3d5ed
Murat Altun <mraltun@gmail.com>
2023-02-05 13:58:16 +0300

    Added composition

--- 6) Functional Programming and React/App.jsx
+++ 6) Functional Programming and React/App.jsx
@@ -1,4 +1,5 @@
 import { RecursiveComponent } from "./RecursiveComponent";
+import { DangerButton, BigSuccessButton } from "./composition";
 
 const nestedObject = {
   a: 1,
@@ -23,7 +24,13 @@ const nestedObject = {
 };
 
 const App = () => {
-  return <RecursiveComponent data={nestedObject} />;
+  return (
+    <>
+      <RecursiveComponent data={nestedObject} />
+      <DangerButton text="Don't do it!" />
+      <BigSuccessButton text='Yes!!!' />
+    </>
+  );
 };
 
 export default App;
